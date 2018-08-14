57109080aa7966badb84ede925f8cae0ef1019b3
Karinna Iniguez <karinnainiguez@gmail.com>
2018-08-13 19:54:06 -0700

    add method for linked list to insert new node at beginning of list

--- data-structures/src/LinkedList.java
+++ data-structures/src/LinkedList.java
@@ -4,19 +4,25 @@ public class LinkedList<X> {
 	public static void main(String[] args) {
 		System.out.println("IT'S WORKING");
 	}
-	
+
 	private Node head;
-	
+
 	public LinkedList() {
 		head = null;
 	}
-	
-	
+
+	// ADD NEW NODE TO LIST FROM INPUT DATA
+	public void add(X data) {
+		Node newNode = new Node(data);
+		newNode.setNextNode(head);
+		head = newNode;
+	}
+
 	private class Node {
-		
+
 		private Node nextNode;
 		private X data;
-		
+
 		public Node(X item) {
 			this.nextNode = null;
 		}
@@ -36,8 +42,7 @@ public class LinkedList<X> {
 		public void setData(X data) {
 			this.data = data;
 		}
-		
-		
+
 	}
 
 }
