19069857b0a75b9fcbe21bfc62b9dddd8a3b2195
Karinna Iniguez <karinnainiguez@gmail.com>
2018-08-13 21:29:22 -0700

    list methods for search, count, and value at n

--- data-structures/src/LinkedList.java
+++ data-structures/src/LinkedList.java
@@ -6,10 +6,13 @@ public class LinkedList<X> {
 	}
 
 	private Node head;
-
+	
+	// CONSTRUCTOR 
 	public LinkedList() {
 		head = null;
 	}
+	
+	// FUNCTIONS
 
 	// ADD NEW NODE TO LIST FROM INPUT DATA
 	public void add(X data) {
@@ -17,7 +20,56 @@ public class LinkedList<X> {
 		newNode.setNextNode(head);
 		head = newNode;
 	}
-
+	
+	// SEARCH FOR NODE
+	public boolean search(X data) {
+		Node current = head;
+		while (current != null) {
+			if(current.data.equals(data)) {
+				return true;
+			} else {
+				current = current.nextNode;
+			}
+		}
+		return false;
+	}
+	
+	// RETURN COUNT OF NODES
+	public int count() {
+		Node current = head;
+		int count = 0;
+		while(current != null) {
+			count += 1;
+			current = current.nextNode;
+		}
+		return count;
+	}
+	
+	// RETURN VALUE AT THE NTH NODE FROM BEGINNING
+	public X valueAt(int n) {
+		int count = 0;
+		Node current = head;
+		while(current != null) {
+			count += 1;
+			current = current.nextNode;
+		}
+	}
+	
+	// MAX VALUE IN THE LINKED LIST RETURNS DATA VALUE, NOT NODE
+//	public X max() {
+//		X max = head.data;
+//		Node current = head.nextNode;
+//		while (current != null) {
+//			if(current.data > max) {
+//				max = current.data;
+//			}
+//			current = current.nextNode;
+//		}
+//		return max;
+//	}
+	
+	
+	// NODE CLASS
 	private class Node {
 
 		private Node nextNode;
