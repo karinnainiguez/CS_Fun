15de20c8537b19379ad71aac2dcbd0b9f69fef5b
Karinna Iniguez <karinnainiguez@gmail.com>
2018-08-13 19:04:38 -0700

    Add nextNode and data to Node class with getters and setters

--- data-structures/src/LinkedList.java
+++ data-structures/src/LinkedList.java
@@ -9,7 +9,31 @@ public class LinkedList<X> {
 	
 	
 	private class Node {
+		
+		private Node nextNode;
+		private X data;
+		
+		public Node(X item) {
+			this.nextNode = null;
+		}
 
+		public Node getNextNode() {
+			return nextNode;
+		}
+
+		public void setNextNode(Node nextNode) {
+			this.nextNode = nextNode;
+		}
+
+		public X getData() {
+			return data;
+		}
+
+		public void setData(X data) {
+			this.data = data;
+		}
+		
+		
 	}
 
 }
