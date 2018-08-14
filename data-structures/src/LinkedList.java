a6f9e64be3fa710e5bda3b18e7e1d72224718ddd
Karinna Iniguez <karinnainiguez@gmail.com>
2018-08-13 18:59:52 -0700

    Add private node class to linked list

--- data-structures/src/LinkedList.java
+++ data-structures/src/LinkedList.java
@@ -1,8 +1,15 @@
 
-public class LinkedList {
+public class LinkedList<X> {
 
 	public static void main(String[] args) {
 		System.out.println("IT'S WORKING");
+	}
+	
+	public LinkedList() {}
+	
+	
+	private class Node {
+
 	}
 
 }
