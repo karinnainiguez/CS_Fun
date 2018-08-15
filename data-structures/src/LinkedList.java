8bd50e7bd813e209cb9e4c4be364324e60217e54
Karinna Iniguez <karinnainiguez@gmail.com>
2018-08-14 19:39:55 -0700

    Add comments with additional methods to implement

--- data-structures/src/LinkedList.java
+++ data-structures/src/LinkedList.java
@@ -4,6 +4,34 @@ public class LinkedList<X> {
 	public static void main(String[] args) {
 		System.out.println("IT'S WORKING");
 	}
+	
+	// NODE CLASS
+		private class Node {
+
+			private Node nextNode;
+			private X data;
+
+			public Node(X item) {
+				this.nextNode = null;
+			}
+
+			public Node getNextNode() {
+				return nextNode;
+			}
+
+			public void setNextNode(Node nextNode) {
+				this.nextNode = nextNode;
+			}
+
+			public X getData() {
+				return data;
+			}
+
+			public void setData(X data) {
+				this.data = data;
+			}
+
+		}
 
 	private Node head;
 	
@@ -12,16 +40,18 @@ public class LinkedList<X> {
 		head = null;
 	}
 	
+	
+	
 	// FUNCTIONS
 
-	// ADD NEW NODE TO LIST FROM INPUT DATA
+	// 1. ADD NEW NODE TO LIST FROM INPUT DATA
 	public void add(X data) {
 		Node newNode = new Node(data);
 		newNode.setNextNode(head);
 		head = newNode;
 	}
 	
-	// SEARCH FOR NODE
+	// 2. SEARCH FOR NODE WITH SPECIFIC INPUT DATA
 	public boolean search(X data) {
 		Node current = head;
 		while (current != null) {
@@ -34,29 +64,8 @@ public class LinkedList<X> {
 		return false;
 	}
 	
-	// RETURN COUNT OF NODES
-	public int count() {
-		Node current = head;
-		int count = 0;
-		while(current != null) {
-			count += 1;
-			current = current.nextNode;
-		}
-		return count;
-	}
-	
-	// RETURN VALUE AT THE NTH NODE FROM BEGINNING
-	public X valueAt(int n) {
-		int count = 0;
-		Node current = head;
-		while(count != n) {
-			count += 1;
-			current = current.nextNode;
-		}
-		return current.data;
-	}
-	
-	// MAX VALUE IN THE LINKED LIST RETURNS DATA VALUE, NOT NODE
+//	// 3. MAX VALUE IN THE LINKED LIST RETURNS DATA VALUE, 
+//	// NOT NODE (NOT applicable in Java with X data)
 //	public X max() {
 //		X max = head.data;
 //		Node current = head.nextNode;
@@ -69,33 +78,66 @@ public class LinkedList<X> {
 //		return max;
 //	}
 	
+//		// 4. MIN VALUE IN THE LINKED LIST RETURNS DATA VALUE, 
+//		// NOT NODE (NOT applicable in Java with X data)
+//		public X min() {
+//			X min = head.data;
+//			Node current = head.nextNode;
+//			while (current != null) {
+//				if(current.data < min) {
+//					min = current.data;
+//				}
+//				current = current.nextNode;
+//			}
+//			return min;
+//		}
 	
-	// NODE CLASS
-	private class Node {
-
-		private Node nextNode;
-		private X data;
-
-		public Node(X item) {
-			this.nextNode = null;
+	
+	// 5. RETURN COUNT OF NODES
+	public int count() {
+		Node current = head;
+		int count = 0;
+		while(current != null) {
+			count += 1;
+			current = current.nextNode;
 		}
-
-		public Node getNextNode() {
-			return nextNode;
-		}
-
-		public void setNextNode(Node nextNode) {
-			this.nextNode = nextNode;
-		}
-
-		public X getData() {
-			return data;
-		}
-
-		public void setData(X data) {
-			this.data = data;
-		}
-
+		return count;
 	}
+	
+	// 6. RETURN VALUE AT THE NTH NODE FROM BEGINNING
+	public X valueAt(int n) {
+		int count = 0;
+		Node current = head;
+		while(count != n) {
+			count += 1;
+			current = current.nextNode;
+		}
+		return current.data;
+	}
+	
+	
+	// 7. INSERT A NEW NODE WITH SPECIFIC DATA 
+	// ASSUMING LIST IS SORTED BY ASCENDING VALUES
+	
+	
+	
+	// 8. PRINT VALUE OF EACH NODE SEPARATED BY SPACE
+	
+	// 9. DELETE FIRST NODE FOUND WITH SPECIFIC DATA
+	
+	// 10. REVERSE LIST (NODES SHOULD BE PRESERVED)
+	
+	// 11. RETURN VALUE AT MIDDLE NODE.  IF NODE COUNT
+	// IS EVEN, PICK ONE OF THE TWO MIDDLE VALUES TO RETURN
+	
+
+	// 12. FIND NTH NODE FROM END AND RETURN VALUE.
+
+	
+	// 13. CHECK IF LIST HAS A CYCLE. CYCLE EXISTS IF ANY
+	// NODE IN LIST LINKS TO A NODE ALREADY VISITED. 
+	// RETURNS BOOLEAN
+
+	
 
 }
